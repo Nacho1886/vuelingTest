d9a135e59615548924cc6f3b493510a3ba46304e
Nacho1886 <francert@hotmail.com>
2023-02-06 14:13:32 +0100

    Masonry in process

--- src/app/home/services/unspash-api.service.ts
+++ src/app/home/services/unspash-api.service.ts
@@ -1,25 +1,26 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { PictureData } from '../interfaces/picture-data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UnspashApiService {
 
-  constructor(private http: HttpClient) {
-    
-  }
+  apiUrl:string = 'https://api.unsplash.com/photos/';
+
+  options: object = {
+    headers: {
+      Authorization: 'Client-ID KfHoL6jNCXagd2FKrfo5LECINgGoAupSjzzhKc3P-i8',
+    },
+  };
+
+  constructor(private http: HttpClient) {}
+
+
   getPageUnsplash(): Observable<PictureData[]> {
-
-    const randomNumber = Math.round(Math.random() * 400)
-    const options = {
-      headers: {
-        Authorization: 'Client-ID KfHoL6jNCXagd2FKrfo5LECINgGoAupSjzzhKc3P-i8'
-      }
-    }
-
-    return this.http.get<PictureData[]>(`https://api.unsplash.com/photos?page=${randomNumber}`, options)
+    return this.http.get<PictureData[]>(`${this.apiUrl}random?count=12`, this.options);
   }
 }
